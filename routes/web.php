b3faad551ae2c19198e9cf914ca0820c69c6b74b
dinommag <163142789+dinommag@users.noreply.github.com>
2026-05-11 17:00:05 +0800

    Continue Admin Layouting

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Admin\AdminController;
 
 Route::get('/', [HomeController::class, 'index']);
+
+// Admin routes moved to routes/admin.php
